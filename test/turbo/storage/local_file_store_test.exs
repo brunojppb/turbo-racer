e19aa4587a9190bd129bb84019fcd97b18aa32fb
Bruno Paulino <bruno@bpaulino.com>
2022-08-11 21:55:33 +0200

    make tests async

--- test/turbo/storage/local_file_store_test.exs
+++ test/turbo/storage/local_file_store_test.exs
@@ -1,5 +1,5 @@
 defmodule Turbo.Storage.LocalFileStoreTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
   alias Turbo.Storage.FileStore
 
   defp get_temp_file(filename) do
@@ -15,10 +15,6 @@ defmodule Turbo.Storage.LocalFileStoreTest do
       tmp_file_path = get_temp_file(hash)
 
       {:ok, ^hash} = FileStore.put_file(tmp_file_path, hash)
-    end
-
-    test "delete file from local filesystem" do
-      hash = "0da9e30c6776aa43"
       {:ok, ^hash} = FileStore.delete_file(hash)
     end
 
@@ -26,10 +22,13 @@ defmodule Turbo.Storage.LocalFileStoreTest do
       hash = "0da9e30c6776aa44"
       binary_content = "i_am_some_binary_content"
       {:ok, ^hash} = FileStore.put_data(binary_content, hash)
+      {:ok, ^hash} = FileStore.delete_file(hash)
     end
 
     test "get binary file as a stream" do
-      hash = "0da9e30c6776aa44"
+      hash = "0da9e30c6776aa445"
+      binary_content = "i_am_some_binary_content"
+      {:ok, ^hash} = FileStore.put_data(binary_content, hash)
       {:ok, stream} = FileStore.get_file(hash)
       content = Enum.join(stream, "")
       assert content == "i_am_some_binary_content"
