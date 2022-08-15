8f7a519fa8302b1f7d02f80a918e242b27408dc0
Bruno Paulino <bruno@bpaulino.com>
2022-08-15 23:28:07 +0200

    Redirect index to teams if logged in

--- lib/turbo_web/controllers/page_controller.ex
+++ lib/turbo_web/controllers/page_controller.ex
@@ -2,6 +2,9 @@ defmodule TurboWeb.PageController do
   use TurboWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    case conn.assigns[:current_user] do
+      nil -> render(conn, "index.html")
+      _ -> redirect(conn, to: Routes.team_path(conn, :index))
+    end
   end
 end
