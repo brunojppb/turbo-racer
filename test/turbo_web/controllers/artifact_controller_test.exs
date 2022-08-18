fc616e8775fa6e2a74071c993fa602a3900082f1
Bruno Paulino <bruno@bpaulino.com>
2022-08-18 23:52:40 +0200

    Test for unauthorized cases

--- test/turbo_web/controllers/artifact_controller_test.exs
+++ test/turbo_web/controllers/artifact_controller_test.exs
@@ -18,29 +18,52 @@ defmodule TurboWeb.ArtifactControllerTest do
     {:ok, %{artifact: artifact}}
   end
 
-  test "PUT /v8/artifacts/:hash should return the stored artifact name", %{conn: conn, team: team} do
-    hash = Turbo.generate_rand_token()
+  describe "PUT /v8/artifacts/:hash" do
+    test "should return the stored artifact name", %{conn: conn, team: team} do
+      hash = Turbo.generate_rand_token()
 
-    conn =
-      conn
-      |> put_req_header("content-type", "application/octet-stream")
-      |> put("/v8/artifacts/#{hash}?slug=#{team.name}", "some_binary_data")
+      conn =
+        conn
+        |> put_req_header("content-type", "application/octet-stream")
+        |> put("/v8/artifacts/#{hash}?slug=#{team.name}", "some_binary_data")
 
-    assert %{"filename" => ^hash} = json_response(conn, 201)
-    ArtifactsFixtures.clean_up_artifact(hash)
+      assert %{"filename" => ^hash} = json_response(conn, 201)
+      ArtifactsFixtures.clean_up_artifact(hash)
+    end
+
+    test "return unauthorized if Bearer token isn't present" do
+      conn =
+        build_conn()
+        |> put_req_header("content-type", "application/octet-stream")
+        |> put("/v8/artifacts/random-hash?slug=team", "some_binary_data")
+
+      assert response(conn, 401)
+    end
   end
 
-  test "GET /v8/artifacts/:hash should return the artifact binary", %{
-    conn: conn,
-    team: team,
-    artifact: artifact
-  } do
-    conn = get(conn, "/v8/artifacts/#{artifact.hash}?slug=#{team.name}")
-    assert response(conn, 200)
+  describe "GET /v8/artifacts/:hash" do
+    test "should return the artifact binary", %{
+      conn: conn,
+      team: team,
+      artifact: artifact
+    } do
+      conn = get(conn, "/v8/artifacts/#{artifact.hash}?slug=#{team.name}")
+      assert response(conn, 200)
+    end
+
+    test "return unauthorized if Bearer token isn't present" do
+      conn =
+        build_conn()
+        |> get("/v8/artifacts/random-hash?slug=team")
+
+      assert response(conn, 401)
+    end
   end
 
-  test "POST /v8/artifacts/events should respond with an empty JSON", %{conn: conn} do
-    conn = post(conn, "/v8/artifacts/events")
-    assert json_response(conn, 201) == %{}
+  describe "POST /v8/artifacts/events" do
+    test "should respond with an empty JSON", %{conn: conn} do
+      conn = post(conn, "/v8/artifacts/events")
+      assert json_response(conn, 201) == %{}
+    end
   end
 end
