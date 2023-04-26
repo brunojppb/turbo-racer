7151303da4af78aa64f297d4bf328d4a75f1711a
Bruno Paulino <bruno@bpaulino.com>
2023-04-26 08:46:03 +0200

    Bump to version 1.4.1

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Turbo.MixProject do
     [
       app: :turbo,
       name: "Turbo Racer",
-      version: "1.4.0",
+      version: "1.4.1",
       elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: Mix.compilers(),
