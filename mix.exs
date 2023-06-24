8a0b02b351a3458c23c1496f97c8211bab6958fe
Bruno Paulino <bruno@bpaulino.com>
2023-06-24 14:33:49 +0200

    Bump elixir to 1.15 and Erlang to 26

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Turbo.MixProject do
       app: :turbo,
       name: "Turbo Racer",
       version: "1.4.1",
-      elixir: "~> 1.14",
+      elixir: "~> 1.15",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: Mix.compilers(),
       start_permanent: Mix.env() == :prod,
